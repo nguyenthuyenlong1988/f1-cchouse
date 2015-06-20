36f1ff19fb2b15bf43e88d9eeaa0b338a94c7546
Tien Nguyen <htien@users.noreply.github.com>
2015-06-20 23:15:59 +0700

    Fix một số lỗi về đặt tên file.
    Signed-off-by: Tien Nguyen <htien@users.noreply.github.com>

--- database/migrations/2015_06_20_075611_create_subjects_table.php
+++ database/migrations/2015_06_20_075611_create_subjects_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSubjectTable extends Migration
+class CreateSubjectsTable extends Migration
 {
   /**
    * Run the migrations.
@@ -12,10 +12,10 @@ class CreateSubjectTable extends Migration
    */
   public function up()
   {
-    Schema::create( 'subject', function ( Blueprint $table ) {
-      $table->string('id', 36 )->primary();
-      $table->string('name', 50 );
-    } );
+    Schema::create('subjects', function (Blueprint $table) {
+      $table->string('id', 36)->primary();
+      $table->string('name', 50);
+    });
   }
 
   /**
@@ -25,6 +25,6 @@ class CreateSubjectTable extends Migration
    */
   public function down()
   {
-    Schema::drop( 'subject' );
+    Schema::drop('subjects');
   }
 }
