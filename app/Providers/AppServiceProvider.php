de98ba5e2eebcb36b2ae4cdbb52388fd531bc012
Tien Nguyen <htien@users.noreply.github.com>
2015-07-30 22:59:59 +0700

    Tạo DB Listener để capture sql query.
    Signed-off-by: Tien Nguyen <htien@users.noreply.github.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace NhaThieuNhi\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,7 +17,17 @@ class AppServiceProvider extends ServiceProvider
         $sharedVars = [
             'ps' => config('params')
         ];
+
         view()->share($sharedVars);
+
+        \DB::listen(function ($sql, $bindings, $time) {
+            $output = new ConsoleOutput();
+            $output->writeln('=====>>>>>>>>>>');
+            $output->writeln("<info>Query:</info> $sql");
+            $output->writeln('<info>Bindings:</info> ' . print_r($bindings, TRUE));
+            $output->writeln("<info>Time:</info> $time");
+            $output->writeln('<<<<<<<<<<=====');
+        });
     }
 
     /**
