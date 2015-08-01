b6c47b53e51c673a95e7046857c9f191991e9dd6
Tien Nguyen <htien@users.noreply.github.com>
2015-08-01 13:55:57 +0700

    Thêm title cho Facebook fanpage vị trí sidebar.
    Signed-off-by: Tien Nguyen <htien@users.noreply.github.com>

--- resources/views/_shared/common/facebook_fanpage.blade.php
+++ resources/views/_shared/common/facebook_fanpage.blade.php
@@ -5,9 +5,12 @@
   <div class="fb-xfbml-parse-ignore"></div>
 </div>
 @else
-  <div class="box">
+<div class="box">
+  <h4 class="box-title"><span>Fanpage</span></h4>
+  <div>
     <div class="fb-page" data-href="https://www.facebook.com/nhathieunhigovap2004" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="true">
       <div class="fb-xfbml-parse-ignore"></div>
     </div>
   </div>
+</div>
 @endif
