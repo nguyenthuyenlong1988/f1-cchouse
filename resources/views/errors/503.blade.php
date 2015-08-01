ee623a3612696031bac062caf8ed2243968fdb51
Tien Nguyen <htien@users.noreply.github.com>
2015-08-02 06:40:56 +0700

    Chỉnh giao diện trang 503 (maintaince page).
    Signed-off-by: Tien Nguyen <htien@users.noreply.github.com>

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="UTF-8" />
-  <title>Bảo trì.</title>
+  <title>Bảo trì &mdash; Nhà Thiếu Nhi Gò Vấp</title>
   <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Alegreya+Sans:300,400&subset=latin,vietnamese' />
   <style>
     html, body {
@@ -25,15 +25,21 @@
 
     .header {
       display: table-header-group;
-      background-color: #64b862;
       text-align: center;
     }
 
     .header h1 {
-      margin: 8px 0;
+      margin: 0 0 25px;
+      padding: 20px 10px;
       font-size: 40px;
       font-weight: 300;
       color: #fff;
+      background-color: #017275;
+    }
+
+    .footer {
+      display: table-footer-group;
+      text-align: center;
     }
 
     .fb-page {
@@ -55,13 +61,28 @@
       font-size: 72px;
       margin-bottom: 40px;
     }
+
+    #copyright {
+      padding: 15px 10px;
+      background-color: #017275;
+      color: #fff;
+    }
+
+    #copyright a {
+      color: #b5eaea;
+      text-decoration: none;
+    }
+
+    #copyright a:hover {
+      border-bottom: 1px solid #b5eaea;
+    }
   </style>
 </head>
 <body>
   @include('_layouts.home.inc.billy_neck')
 
   <div class="header">
-    <h1>Nhà Thiếu Nhi Gò Vấp</h1>
+    <h1>Nhà Thiếu Nhi Gò Vấp <br /> TP.HCM</h1>
     <div class="fb-page hidden-xs" data-href="https://www.facebook.com/nhathieunhigovap2004" data-hide-cover="false" data-show-facepile="false" data-show-posts="false">
       <div class="fb-xfbml-parse-ignore"></div>
     </div>
@@ -71,5 +92,12 @@
       <div class="title">Đang bảo trì.</div>
     </div>
   </div>
+  <div class="footer">
+    <div id="copyright" class="billy-copyright">
+      <span>&copy; 2015</span> &ndash;
+      <span>Bản quyền thuộc về Nhà Thiếu Nhi Gò Vấp. Phát triển bởi <a href="http://hva.io">HVA Team</a>.</span>
+    </div>
+  </div>
+
 </body>
 </html>
