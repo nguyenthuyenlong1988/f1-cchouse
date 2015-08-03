73c04aada74d68ad6a351fbddd255602e0102a9b
Tien Nguyen <htien@users.noreply.github.com>
2015-08-03 08:38:07 +0700

    [Nghiêm trọng] Đã sửa lỗi tự động trả về tập tin html khi truy cập web.
    Signed-off-by: Tien Nguyen <htien@users.noreply.github.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,17 +20,19 @@ class AppServiceProvider extends ServiceProvider
 
         view()->share($sharedVars);
 
-        \DB::listen(function ($sql, $bindings, $time) {
-            $output = new ConsoleOutput();
-            $output->writeln('');
-            $output->writeln('');
-            $output->writeln('=====>>>>>>>>>>');
-            $output->writeln("<info>Query:</info> $sql");
-            $output->writeln('<info>Bindings:</info> ' . print_r($bindings, TRUE));
-            $output->writeln("<info>Time:</info> $time");
-            $output->writeln('');
-            $output->writeln('');
-        });
+        if (env('APP_DEBUG')) {
+            \DB::listen(function ($sql, $bindings, $time) {
+                $output = new ConsoleOutput();
+                $output->writeln('');
+                $output->writeln('');
+                $output->writeln('=====>>>>>>>>>>');
+                $output->writeln("<info>Query:</info> $sql");
+                $output->writeln('<info>Bindings:</info> ' . print_r($bindings, TRUE));
+                $output->writeln("<info>Time:</info> $time");
+                $output->writeln('');
+                $output->writeln('');
+            });
+        }
     }
 
     /**
